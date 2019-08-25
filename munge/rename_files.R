5e3fee9b247a113df3c215a718be1e2328abdd88
Joe Powers <joepowers16@gmail.com>
2019-08-25 14:52:09 -0700

    adds code to replace strings in file names

--- munge/rename_files.R
+++ munge/rename_files.R
@@ -1,4 +1,9 @@
+library(tidyverse)
+
 getwd()
+
+## Section: find and replace string in file name  ----
+##############################################################################-
 
 myfiles <- list.files(recursive = TRUE) 
 
@@ -9,4 +14,19 @@ files_to_rename
 file.rename(
   from = files_to_rename, 
   to = str_replace(files_to_rename, "drinks_agg", "drinks_latlong_gdp")
+)
+
+## Section: find and replace string in multiple file names   ----
+##############################################################################-
+
+myfiles <- list.files(recursive = TRUE) 
+
+files_to_rename <- myfiles[(str_detect(myfiles, "drinks"))]
+
+files_to_rename
+
+# Note that file.rename is vectorized
+file.rename(
+  from = files_to_rename, 
+  to = str_replace(files_to_rename, "drinks", "alcohol")
 )
